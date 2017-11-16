041c45cc6e97e6c976fd11169e8fc1307ae297da
Jack Nudelman <jack.nudel@gmail.com>
2017-11-16 16:13:44 -0500

    bug

--- data_extraction.py
+++ data_extraction.py
@@ -10,7 +10,6 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
-matplotlib.use('Agg')
 import numpy as np
 from torch.autograd import Variable
 # import pandas as pd
