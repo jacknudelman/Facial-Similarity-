947f9bdd1f059cda556d96d924e95f5ab96ee990
Jack Nudelman <jack.nudel@gmail.com>
2017-11-20 15:17:18 -0500

    new stuff

--- p1a.py
+++ p1a.py
@@ -200,14 +200,14 @@ for epoch in range(4):
         out = net(Variable(sample_batch['image1'], requires_grad=True).cuda(), Variable(sample_batch['image2'], requires_grad=True).cuda())
         target = sample_batch['label']
         target = np.array([float(i) for i in target])
-        print out
-        print out.data[0][0]
-        print type(out.data[0][0])
-        # print out.data[0][1]
-        print out.data[1][0]
+        # print out
+        # print out.data[0][0]
+        # print type(out.data[0][0])
+        # # print out.data[0][1]
+        # print out.data[1][0]
         # out_arr = out.data.cpu().numpy()
         for i in range(net.batchSize):
-            num_correctly_matched = num_correctly_matched + 1 if((target[i] == 1 and out.index_select(0,i) >= 0.5) or (target[i] == 0 and out.index_select(0,i) < 0.5)) else num_correctly_matched
+            num_correctly_matched = num_correctly_matched + 1 if((target[i] == 1 and out[i][0] >= 0.5) or (target[i] == 0 and out[i][0] < 0.5)) else num_correctly_matched
         print 'num_correctly_matched = ', num_correctly_matched
         num_images += target.shape[0]
         target = torch.from_numpy(target).view(target.shape[0], -1)
