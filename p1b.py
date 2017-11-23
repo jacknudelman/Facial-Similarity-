9a02501b98a12f22423b1cbd9a3029bae585838c
Jack Nudelman <jack.nudel@gmail.com>
2017-11-23 16:52:49 -0500

    l

--- p1b.py
+++ p1b.py
@@ -86,6 +86,7 @@ class ContrastiveLoss(nn.Module):
 
     def forward(self, input1, input2, target):
         distance = torch.sqrt(torch.pow(input1, 2) - torch.pow(input2, 2))
+        print distance
         return torch.mean((target) * torch.pow(distance, 2) + (1 - target) * torch.pow(torch.clamp(self.margin - distance, min=0.0), 2))
 
 def compute_test_loss(net, dataloader):
