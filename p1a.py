92c27c3d03c0af022b0d676c259c491bb06760ff
Jack Nudelman <jack.nudel@gmail.com>
2017-11-24 18:44:45 -0500

    running for more epochs

--- p1a.py
+++ p1a.py
@@ -237,10 +237,10 @@ print len(testing_loss_list)
 print len(average_testing_loss)
 
 x_training = np.linspace(0, iter_num, len(training_loss_list))
-plt.plot(x_training, training_loss_list)
+plt.plot(x_training, training_loss_list, label='training loss')
 
 x_raw_testing = np.linspace(0, iter_num, len(testing_loss_list))
-plt.plot(x_raw_testing, testing_loss_list)
+plt.plot(x_raw_testing, testing_loss_list, label='testing loss')
 
 plt.savefig(file_name)
 plt.title('losses')
