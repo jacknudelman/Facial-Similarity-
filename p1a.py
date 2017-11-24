8236dbfca236b25499c89525941f4aa445e839a4
Jack Nudelman <jack.nudel@gmail.com>
2017-11-24 13:58:37 -0500

    l

--- p1a.py
+++ p1a.py
@@ -170,7 +170,7 @@ if ('--augment' in sys.argv):
     file_name = 'aug_fig'
     train_face_dataset = RandFaceDataset(csv_file='test.txt', root_dir='lfw/', transform=test_transformation)
     train_dataloader = DataLoader(train_face_dataset, batch_size=net.batchSize, shuffle=True, num_workers=net.batchSize)
-for epoch in range(10):
+for epoch in range(2):
     print epoch
     num_images = 0
 
