a917333a6ab5369a99f2980e22755bb264409aef
Jack Nudelman <jack.nudel@gmail.com>
2017-11-26 16:12:30 -0500

    added eval

--- p1a.py
+++ p1a.py
@@ -249,13 +249,13 @@ def train(weight_path):
 
 
     net.eval()
-    train_transformation = transforms.Compose([transforms.Scale((128, 128)), transforms.ToTensor()])
-    train_face_dataset = FaceDataset(csv_file='train.txt', root_dir='lfw/', transform=train_transformation)
-    train_dataloader = DataLoader(train_face_dataset, batch_size=net.batchSize, shuffle=True, num_workers=net.batchSize)
-
-    test_transformation = transforms.Compose([transforms.Scale((128, 128)), transforms.ToTensor()])
-    test_face_dataset = RandFaceDataset(csv_file='test.txt', root_dir='lfw/', transform=test_transformation)
-    test_dataloader = DataLoader(test_face_dataset, batch_size=net.batchSize, shuffle=True, num_workers=net.batchSize)
+    # train_transformation = transforms.Compose([transforms.Scale((128, 128)), transforms.ToTensor()])
+    # train_face_dataset = FaceDataset(csv_file='train.txt', root_dir='lfw/', transform=train_transformation)
+    # train_dataloader = DataLoader(train_face_dataset, batch_size=net.batchSize, shuffle=True, num_workers=net.batchSize)
+    #
+    # test_transformation = transforms.Compose([transforms.Scale((128, 128)), transforms.ToTensor()])
+    # test_face_dataset = RandFaceDataset(csv_file='test.txt', root_dir='lfw/', transform=test_transformation)
+    # test_dataloader = DataLoader(test_face_dataset, batch_size=net.batchSize, shuffle=True, num_workers=net.batchSize)
 
     learning_rate = 1e-6
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
