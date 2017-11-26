e96366a50e5309290b393c19a9e8c9f021100600
Jack Nudelman <jack.nudel@gmail.com>
2017-11-25 20:50:35 -0500

    g

--- p1a.py
+++ p1a.py
@@ -258,7 +258,7 @@ if '--load' in sys.argv:
     net.load_state_dict(torch.load(weight_path))
     # print 'created net'
     train_transformation = transforms.Compose([transforms.Scale((128, 128)), transforms.ToTensor()])
-    train_face_dataset = FaceDataset(csv_file='train.txt', root_dir='lfw/', transform=train_transformation)
+    train_face_dataset = RandFaceDataset(csv_file='train.txt', root_dir='lfw/', transform=train_transformation)
     train_dataloader = DataLoader(train_face_dataset, batch_size=net.batchSize, shuffle=False, num_workers=net.batchSize)
 
     test_transformation = transforms.Compose([transforms.Scale((128, 128)), transforms.ToTensor()])
